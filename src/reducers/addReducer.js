c7fc179f75d6d86d14f31319c41028746c6146fc
William Ryan <billy.ryan.rom@gmail.com>
2020-02-19 03:00:17 -0500

    Add Store with Reducers to index.js src FIXED

---
+++ src/reducers/addReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+    features: []
+};
+
+export const addReducer = (state = initialState, action) => {
+    switch (action.type){
+        case 'ADD_FEATURE': 
+        default: return state;
+    }
+}
